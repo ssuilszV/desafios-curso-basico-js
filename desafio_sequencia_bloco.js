840a0218c4c7349afa8dd6c9fb6c7d0d0d3ffd6e
Louis H <94133351+ssuilszV@users.noreply.github.com>
2022-06-10 15:05:04 -0300

    Add files via upload

---
+++ desafio_sequencia_bloco.js
@@ -0,0 +1,25 @@
+/* 
+Desafio que consiste em mostrar em uma lista organizada, os numeros ÍMPARES de 1 á 10, e consecutivamente, uma lista com os números PARES, também de 1 á 10.
+
+*/
+ 
+
+//Bloco usado para manter os números impares
+
+{
+    console.log("1");
+    console.log("3");
+    console.log("5");
+    console.log("7");
+    console.log("9");
+}
+
+//Bloco usando para manter os números pares
+
+{
+    console.log("\n2");
+    console.log("4");
+    console.log("6");
+    console.log("8");
+    console.log("10");
+}
